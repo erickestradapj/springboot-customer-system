15f4885dff1d113c1fd7b2d955e370ab9854105b
Erick Estrada <erickestradapj@gmail.com>
2021-10-12 11:37:42 -0500

    starting project

---
+++ src/main/java/com/erickestradapj/system/app/FacturationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.erickestradapj.system.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erickestradapj/system/app/FacturationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickestradapj.system.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
